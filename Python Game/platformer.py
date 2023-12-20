71b16181d6cb1770246ee9fe16a0cd65ed1af418
Cwilmeth91 <cwilmeth91@yahoo.com>
2023-12-20 11:33:26 -0600

    Fixing Jump,Added win Label and Btn to Play again

--- Python Game/platformer.py
+++ Python Game/platformer.py
@@ -36,14 +36,14 @@ is_jumping = False
 ground = pygame.Rect(0, HEIGHT - GROUND_HEIGHT, WIDTH, GROUND_HEIGHT)
 
 # Set up coins
-coins = []
-for _ in range(COIN_COUNT):
-    coin = pygame.Rect(random.randint(0, WIDTH - COIN_SIZE), random.randint(0, HEIGHT - GROUND_HEIGHT - COIN_SIZE), COIN_SIZE, COIN_SIZE)
-    coins.append(coin)
+coins = [pygame.Rect(random.randint(0, WIDTH - COIN_SIZE), random.randint(0, HEIGHT - GROUND_HEIGHT - COIN_SIZE), COIN_SIZE, COIN_SIZE) for _ in range(COIN_COUNT)]
 
 # Set up player score
 score = 0
 font = pygame.font.Font(None, 36)
+
+# Variable to track win state
+win = False
 
 # Clock to control the frame rate
 clock = pygame.time.Clock()
@@ -63,7 +63,7 @@ while True:
     if keys[pygame.K_RIGHT] and player.right < WIDTH:
         player.x += player_speed
 
-    # Jumping mechanics
+     # Jumping mechanics
     if not is_jumping:
         if keys[pygame.K_SPACE]:
             is_jumping = True
@@ -79,15 +79,15 @@ while True:
     if player.y < HEIGHT - GROUND_HEIGHT - PLAYER_SIZE:
         player.y += gravity
 
-    # Check for collisions with the ground
-    if player.colliderect(ground):
-        player.y = HEIGHT - GROUND_HEIGHT - PLAYER_SIZE
-
     # Check for collisions with coins
     for coin in coins:
         if player.colliderect(coin):
             coins.remove(coin)
             score += 1
+
+    # Check for win condition
+    if score == COIN_COUNT:
+        win = True
 
     # Clear the screen
     screen.fill(WHITE)
@@ -102,6 +102,27 @@ while True:
     # Draw score
     score_text = font.render(f"Score: {score}", True, (0, 0, 0))
     screen.blit(score_text, (10, 10))
+
+    # Check for win and display message
+    if win:
+        win_text = font.render("You Win!", True, (0, 0, 0))
+        screen.blit(win_text, (WIDTH // 2 - 50, HEIGHT // 2 - 20))
+
+        # Add a "Play Again" button
+        play_again_rect = pygame.Rect(WIDTH // 2 - 60, HEIGHT // 2 + 20, 120, 40)
+        pygame.draw.rect(screen, (0, 255, 0), play_again_rect)  # Green color for the button
+        play_again_text = font.render("Play Again", True, (0, 0, 0))
+        screen.blit(play_again_text, (WIDTH // 2 - 60, HEIGHT // 2 + 30))
+
+        # Check if the "Play Again" button is clicked
+        if play_again_rect.collidepoint(pygame.mouse.get_pos()):
+            if pygame.mouse.get_pressed()[0]:  # 0 corresponds to the left mouse button
+                # Reset the game state
+                coins = [pygame.Rect(random.randint(0, WIDTH - COIN_SIZE), random.randint(0, HEIGHT - GROUND_HEIGHT - COIN_SIZE), COIN_SIZE, COIN_SIZE) for _ in range(COIN_COUNT)]
+                player.x = 50
+                player.y = HEIGHT - GROUND_HEIGHT - PLAYER_SIZE
+                score = 0
+                win = False
 
     # Update the display
     pygame.display.flip()
